f9597f5705d2c0b9b4183b6551809f7f4e6283c7
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2023-04-16 16:01:06 +0000

    Done correction

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -23,7 +23,7 @@ public class Pizza {
     }
 
     public int getPrice(){
-        return this.price;
+        return this.isVeg ? vegBasePrice : nonVegBasePrice;
     }
 
     public void addExtraCheese(){
